9c6c9af8cfee3993453300f91a08887aa9c8071b
“knoweski <Krystle.noweski@gmail.com ~$ git config --global user.name knoweski>
2019-10-21 14:53:09 -0400

    last commit

--- function1.js
+++ function1.js
@@ -15,6 +15,12 @@ function yearOfBirth(age){
 if (age < 0) {
     throw new Error("Age cannot be negative");
 }
+if (name === undefined || age === undefined){
+    throw new Error('Arguments not valid');
+}
+if (typeof age != 'number'){
+    throw new TypeError('Age must be number');
+}
 try {
     const greeting1 = createGreeting('Bob', '35');
     console.log(greeting1);
