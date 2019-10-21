ced93db69d2746c1eb9848208897d8caec0780be
Peter Rufo <peter@peterrufo.com>
2019-10-21 15:15:59 -0400

    beyond function

--- function2.js
+++ function2.js
@@ -1,4 +1,20 @@
+'use strict';
+
 function jediName(firstName, lastName){
     return `${lastName.slice(0,3)}${firstName.slice(0,2)}`;
 }
 
+function beyond(num){
+    if (num === Infinity){
+        console.log('And Beyond');
+    }
+    if (num > 0){
+        console.log('To infinity');
+    }
+    if (num < 0){
+        console.log('To negative infinity');
+    }
+    if (num === 0){
+        console.log('Staying home');
+    }
+}
