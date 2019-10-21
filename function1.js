f4a1250a80b3e7e52d1989ee7fa0b076cadb2638
“knoweski <Krystle.noweski@gmail.com ~$ git config --global user.name knoweski>
2019-10-21 14:28:40 -0400

    yearOfBirth

--- function1.js
+++ function1.js
@@ -1,5 +1,7 @@
 function createGreeting(name,age){
-return 'Hi, my name is Krystle and I\'m 33 years old';
+return `Hi, my name is Krystle and I\'m 33 years old`;
+const yearOfBirth = 2019 - age;
+return `I was born in ${yearOfBirth}.`;
 }
 const greeting1 = createGreeting();
 console.log(greeting1);
